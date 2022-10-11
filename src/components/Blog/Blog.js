e21c2ad283da2e43720655c3f14df6156c2a4ddf
noor16mustafa <c173212@ugrad.iiuc.ac.bd>
2022-10-11 20:56:36 +0600

    blog side added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,7 +3,7 @@ import Accordion from 'react-bootstrap/Accordion';
 
 const Blog = () => {
     return (
-        <Accordion className='my-5 mx-5' >
+        <Accordion className='my-5 mx-5' defaultActiveKey={['0']} alwaysOpen>
             <Accordion.Item eventKey="0">
                 <Accordion.Header> #1 What is the purpose of React Router?</Accordion.Header>
                 <Accordion.Body className='text-start'>
@@ -14,14 +14,14 @@ const Blog = () => {
             </Accordion.Item>
             <Accordion.Item eventKey="1">
                 <Accordion.Header>#2 How does context API works?</Accordion.Header>
-                <Accordion.Body>
-
+                <Accordion.Body className='text-start'>
+                    The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="2">
-                <Accordion.Header>Accordion Item #2</Accordion.Header>
-                <Accordion.Body>
-
+                <Accordion.Header>#3 What is useRef() hooks?</Accordion.Header>
+                <Accordion.Body className='text-start'>
+                    The useRef is a hook that allows to directly create a reference to the DOM element in the functional component. The useRef returns a mutable ref object. This object has a property called current. The value is persisted in the refContainer.current property. These values are accessed from the current property of the returned object. The current property could be initialised to the passed argument initialValue e.g. useRef(initialValue). The object can persist a value for a full lifetime of the component.The useRef hook is a very handy option for both interacting with DOM elements and storing mutating information in your app without triggering a re-render. However, it is suggested to only use the useRef when necessary and within a useEffect to avoid bugs.
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
